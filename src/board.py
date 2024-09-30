57b4ce0cc3445d4e8e311e547d62104b7381c6bd
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-09-30 12:16:36 -0500

    fix: removing screen parameter

--- src/board.py
+++ src/board.py
@@ -64,28 +64,28 @@ class Board:
     # Uses node matrix position to calculate display relative position
     return (self.drawPosition[1] + node[1] * self.nodeSpacing, self.drawPosition[0] + node[0] * self.nodeSpacing)
   
-  def drawSolveButton(self, screen):
+  def drawSolveButton(self):
     button_position = [consts.SCREEN_WIDTH - consts.BUTTON_WIDTH, consts.SCREEN_HEIGHT - consts.BUTTON_HEIGHT]
     button_rect = pygame.Rect(button_position[0], button_position[1], consts.BUTTON_WIDTH, consts.BUTTON_HEIGHT)
     
-    pygame.draw.rect(screen, consts.GREEN, button_rect)
+    pygame.draw.rect(self.screenSurface, consts.GREEN, button_rect)
     font = pygame.font.Font(None, 36)
     text_surface = font.render("Solve", True, consts.BLACK)
     text_rect = text_surface.get_rect(center=button_rect.center)
-    screen.blit(text_surface, text_rect)
+    self.screenSurface.blit(text_surface, text_rect)
   
-  def drawNodes(self, screen):
+  def drawNodes(self):
     # Calculates position for every node and draws a circle with given value
     for u in self.nodes:
-      pygame.draw.circle(screen, consts.WHITE, self.getDrawPosition(u), 16)
-      pygame.draw.circle(screen, consts.BLACK, self.getDrawPosition(u), 16, 1)
-      self.textCache.Draw(screen, self.getDrawPosition(u), self.matrix[u[0]][u[1]])
+      pygame.draw.circle(self.screenSurface, consts.WHITE, self.getDrawPosition(u), 16)
+      pygame.draw.circle(self.screenSurface, consts.BLACK, self.getDrawPosition(u), 16, 1)
+      self.textCache.Draw(self.screenSurface, self.getDrawPosition(u), self.matrix[u[0]][u[1]])
   
-  def drawPossibleEdges(self, screen):
+  def drawPossibleEdges(self):
     for edge in self.possibleEdges:
       source = self.getDrawPosition(edge[1][0])
       destination = self.getDrawPosition(edge[1][1])
-      pygame.draw.line(screen, consts.GRAY, source, destination, 1)
+      pygame.draw.line(self.screenSurface, consts.GRAY, source, destination, 1)
         
   def distanceToEdge(self, point, edge):
     # Unpack the coordinates of the line segment and the point
@@ -153,9 +153,9 @@ class Board:
   def update(self):
     # Function to run with every clock tick
     self.screenSurface.fill(consts.WHITE)
-    self.drawPossibleEdges(self.screenSurface)
-    self.drawNodes(self.screenSurface)
-    self.drawSolveButton(self.screenSurface)
+    self.drawPossibleEdges()
+    self.drawNodes()
+    self.drawSolveButton()
     
     for event in pygame.event.get():
       match event.type:
